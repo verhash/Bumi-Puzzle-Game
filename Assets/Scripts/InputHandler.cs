ef1b345afc85f30ccf01abe6c822ac45a48e6b80
verhash <147616652+verhash@users.noreply.github.com>
2025-05-21 18:34:05 +0200

    First Puzzle
    So I added all the UI and made 2 examples, backpacks and book, when you find(click on) all of them there is a debug "Level finished" as I am not sure what should happen next.

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,9 +1,18 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
 {
     private Camera mainCamera;
+    public int bookAmount = 3;
+    public int backpackAmount = 3;
+
+    public TextMeshProUGUI bookCount;
+    public TextMeshProUGUI backpackCount;
+
+    public GameObject bookCheckmark;
+    public GameObject backpackCheckmark;
 
     void Awake()
     {
@@ -20,6 +29,56 @@ public void OnClick(InputAction.CallbackContext context)
 
         GameObject clickedObject = rayHit.collider.gameObject;
 
-        Destroy(clickedObject);
+        switch (clickedObject.tag)
+        {
+            case "Book":
+                if (bookAmount > 0)
+                {
+                    bookAmount--;
+                    Destroy(clickedObject);
+                }
+                break;
+
+            case "Backpack":
+                if (backpackAmount > 0)
+                {
+                    backpackAmount--;
+                    Destroy(clickedObject);
+                }
+                break;
+        }
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        // Book UI update
+        if (bookAmount <= 0)
+        {
+            bookCount.gameObject.SetActive(false);
+            bookCheckmark.SetActive(true);
+        }
+        else
+        {
+            bookCount.text = bookAmount.ToString();
+        }
+
+        // Backpack UI update
+        if (backpackAmount <= 0)
+        {
+            backpackCount.gameObject.SetActive(false);
+            backpackCheckmark.SetActive(true);
+        }
+        else
+        {
+            backpackCount.text = backpackAmount.ToString();
+        }
+
+        if (bookAmount <=0 && backpackAmount <= 0)
+        {
+            Debug.Log("Level finished!");
+            // You can call SceneManager.LoadScene here when ready
+        }
     }
 }
